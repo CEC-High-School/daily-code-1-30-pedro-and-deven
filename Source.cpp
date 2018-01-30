182e8a7da764eac188d0bb1fec254c89516efd2a
Deven <35495483+SaltyCracker32@users.noreply.github.com>
2018-01-30 07:58:09 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
 
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+	for (int i = 20; i <= 80; ++i)
+		std::cout << i << ' ';
+	
+	for (int i = 70; i >= 10; --i)
+		std::cout << i << ' ';
+
+	int i = 20;
+
+	// while loop execution
+	while (i <= 80) {
+		cout << ' ' << i ;
+		i++;
+
+	}
+
+	i = 70;
+
+	// while loop execution
+	while (i >= 10) {
+		cout << ' ' << i;
+		i--;
+
+	}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+}
